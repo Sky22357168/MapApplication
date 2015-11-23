f45a3be7ed5868bd7da65af6be161d30478f112b
Skytim <sky22357168@hotmail.com>
2015-11-23 09:57:38 +0800

    First commit

---
+++ MapApplications/MapApplications/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MapApplications.Startup))]
+namespace MapApplications
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
